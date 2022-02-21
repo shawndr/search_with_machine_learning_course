adf8e18b0b4f14929709fb94a3728f853b267d3e
Shawn Rutledge <shawn@kfold.org>
2022-02-21 04:14:36 +0000

    Handle dates in different formats for queries Code cleanup

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -4,6 +4,17 @@ from opensearchpy import OpenSearch
 # Create an OpenSearch client instance and put it into Flask shared space for use by the application
 def get_opensearch():
     if 'opensearch' not in g:
-        g.opensearch = OpenSearch(hosts = [{'host': 'localhost', 'port': 9200}], http_compress=True, http_auth=('admin', 'admin'), use_ssl = True,
-                                             verify_certs=False, ssl_assert_hostname=False, ssl_show_warn=False)
+        host = 'localhost'
+        port = 9200
+        auth = ('admin', 'admin') # For testing only. Don't store credentials in code.
+
+        g.opensearch = OpenSearch(
+            hosts = [{'host': host, 'port': port}], 
+            http_compress=True, 
+            http_auth=auth, 
+            use_ssl = True,
+            verify_certs=False, 
+            ssl_assert_hostname=False, 
+            ssl_show_warn=False
+        )
     return g.opensearch
